1c87cea247f4c2b103d84c66acac5f98f390f1e7
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-08-04 00:37:30 +0000

    chore: Add __init__.py

---
+++ {{cookiecutter.repository}}/tasks/__init__.py
@@ -0,0 +1,26 @@
+
+# %% IMPORTS
+
+from invoke import Collection
+
+from . import (
+    checks,
+    cleans,
+    containers,
+    docs,
+    formats,
+    installs,
+)
+
+# %% NAMESPACES
+
+ns = Collection()
+
+# %% COLLECTIONS
+
+ns.add_collection(checks)
+ns.add_collection(cleans)
+ns.add_collection(containers)
+ns.add_collection(docs)
+ns.add_collection(formats)
+ns.add_collection(installs)
